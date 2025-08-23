5b4445aac8482a7b51ba03df5ab549563d56e9e4
Kollyr552 <79722102+Kollyr552@users.noreply.github.com>
2025-08-23 12:23:59 +0200

    normalised bob ypos

--- src/view_bob.gd
+++ src/view_bob.gd
@@ -2,7 +2,7 @@ class_name CameraBob
 extends Node3D
 
 @export var bob_speed: float = 15.0
-@export var bob_height: float = 0.05
+@export var bob_height: float = 0.2
 @export var bob_ease: float = 1.0
 
 var target_bob_ypos: float = 0.0
@@ -10,8 +10,9 @@ var target_bob_ypos: float = 0.0
 func bob_view(time:float, do_bob:bool, d_t:float) -> void:
 	
 	if do_bob:
-		target_bob_ypos = (cos(time*bob_speed) - 1.0) * bob_height
+		target_bob_ypos = (cos(time*bob_speed)-1.0)*0.5 * bob_height
 	else:
 		target_bob_ypos = 0.0
-	
+		
 	position.y = move_toward(position.y, target_bob_ypos, bob_ease*d_t)
+	
