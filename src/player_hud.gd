765ec2ff7d97a955048a08418bd54197fc0e43ad
Kollyr552 <79722102+Kollyr552@users.noreply.github.com>
2025-09-02 19:44:50 +0200

    filesys restructure

--- src/player_hud.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-@onready var h_speed_label: Label = %HSpeedLabel
-@onready var v_speed_label: Label = %VSpeedLabel
-
-func set_speed_labels(vel: Vector3) -> void:
-	v_speed_label.text = ("Vertical: %s" % snappedf(vel.y, 0.01))
-	h_speed_label.text = ("Horizontal: %s" % snappedf(Vector2(vel.x, vel.z).length(), 0.01))
