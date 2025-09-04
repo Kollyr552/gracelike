877906165d10cfb99f6b0aaafbc7b62b58ec9865
Kollyr552 <79722102+Kollyr552@users.noreply.github.com>
2025-09-04 19:19:05 +0200

    example rooms

--- src/scenes/rooms/room_prefab.gd
+++ src/scenes/rooms/room_prefab.gd
@@ -3,5 +3,14 @@ class_name Room extends Node3D
 @onready var marker_startpoint: Marker3D = %MarkerStartpoint
 @onready var marker_endpoint: Marker3D = %MarkerEndpoint
 
+@export var spawn_weight: float = 10.0
+
+func _ready() -> void:
+	marker_startpoint.visible = true
+	marker_endpoint.visible = true
+
 func get_endpoint_position() -> Vector3:
 	return marker_endpoint.global_position
+
+func get_endpoint_rotation() -> Vector3:
+	return marker_endpoint.global_rotation
